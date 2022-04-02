9c011954d5d634b75fc78610aa27f510aedcc2e2
trabalong <gastraba@gmail.com>
2022-04-03 00:48:30 +0200

    commit10

--- src/src.py
+++ src/src.py
@@ -9,3 +9,7 @@ def getGraphic(dataframe):
     plt.figure(figsize=(10, 6))
 
     return sns.heatmap(dataframe.isnull(), yticklabels=False, cmap='viridis', cbar=False)
+
+def getDic(dataframe, column):
+
+    return dict(dataframe[column].value_counts())
